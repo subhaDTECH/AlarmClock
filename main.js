5fd3f73a1c2ddc3e12c479be10c706f772d1348e
subhaDTECH <76237973+subhaDTECH@users.noreply.github.com>
2021-11-29 21:47:49 +0530

    alarmclock

---
+++ main.js
@@ -0,0 +1,24 @@
+document.getElementById('btn').addEventListener('click',(e)=>{
+    e.preventDefault();
+   let givindate= document.getElementById('datevalue').value ;
+   console.log(givindate)
+   let currentdate=new Date();
+   let realgivintime=new Date(givindate);
+   var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3');
+
+   function ringbell(){
+      
+       audio.play();
+       alert("Hey Buddy your alarm ringning")
+   }
+
+   let totaltime=(realgivintime-currentdate);
+   console.log(totaltime)
+   if(totaltime>=0){
+       console.log('finfished');
+       setTimeout(()=>{
+           ringbell();
+           
+       },totaltime)
+   }
+})
